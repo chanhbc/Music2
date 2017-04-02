b62aab040c929d8ede8013c1690d7adaaa40446b
Đỗ Văn Chanh <chanhdv@tapbi.vn>
2017-04-02 14:35:58 +0700

    Initial commit

---
+++ app/src/main/java/music/com/music/App.java
@@ -0,0 +1,19 @@
+package music.com.music;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+
+public class App extends Application {
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this;
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
